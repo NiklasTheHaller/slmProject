ca8b72b70eb5d1d73cf5e2fae473264e8e250535
Niklas <wi22b066@technikum-wien.at>
2023-05-10 16:01:26 +0200

    first commit

---
+++ src/main/java/com/example/slmproject/SlmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmProjectApplication.class, args);
+    }
+
+}
