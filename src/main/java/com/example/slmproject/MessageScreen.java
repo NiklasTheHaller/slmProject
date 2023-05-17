d5175805f20987f06f86d1b65713c6cbacfbc91b
Niklas <wi22b066@technikum-wien.at>
2023-05-17 10:49:51 +0200

    fixed message display and added message test

--- src/main/java/com/example/slmproject/MessageScreen.java
+++ src/main/java/com/example/slmproject/MessageScreen.java
@@ -1,14 +1,16 @@
 package com.example.slmproject;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageScreen {
 
     @RequestMapping("/message")
-    public String msg() {
-        return "Hello World!";
+    public String msg(@RequestParam String msg) {
+
+        return msg;
     }
 
 
